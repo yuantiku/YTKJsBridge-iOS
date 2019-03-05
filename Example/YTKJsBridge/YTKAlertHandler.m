d825a43f0cf4fb7a611317849b913c7a6124de28
lihc <lihc@fenbi.com>
2019-03-05 10:20:41 +0800

    MOD: fix warning.

--- Example/YTKJsBridge/YTKAlertHandler.m
+++ Example/YTKJsBridge/YTKAlertHandler.m
@@ -12,23 +12,29 @@
 
 - (void)syncSayHello:(nullable NSString *)title {
     dispatch_async(dispatch_get_main_queue(), ^{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         UIAlertView *av = [[UIAlertView alloc] initWithTitle: title
                                                      message: nil
                                                     delegate: nil
                                            cancelButtonTitle: @"OK"
                                            otherButtonTitles: nil];
         [av show];
+#pragma clang diagnostic pop
     });
 }
 
 - (void)asyncSayHello:(nullable NSString *)title completion:(void(^)(NSError *error, id value))completion {
     dispatch_async(dispatch_get_main_queue(), ^{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         UIAlertView *av = [[UIAlertView alloc] initWithTitle: title
                                                      message: nil
                                                     delegate: nil
                                            cancelButtonTitle: @"OK"
                                            otherButtonTitles: nil];
         [av show];
+#pragma clang diagnostic pop
         if (completion) {
             completion(nil, nil);
         }
@@ -37,12 +43,15 @@
 
 - (void)alert:(nullable NSString *)title completion:(void(^)(NSError *error, id value))completion {
     dispatch_async(dispatch_get_main_queue(), ^{
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         UIAlertView *av = [[UIAlertView alloc] initWithTitle: title
                                                      message: nil
                                                     delegate: nil
                                            cancelButtonTitle: @"OK"
                                            otherButtonTitles: nil];
         [av show];
+#pragma clang diagnostic pop
         if (completion) {
             completion(nil, nil);
         }
--- YTKJsBridge/Classes/YTKJsBlockHeader.h
+++ YTKJsBridge/Classes/YTKJsBlockHeader.h
@@ -9,7 +9,7 @@
 #define YTKJsBlockHeader_h
 
 typedef void (^YTKDataCallback) (NSError * __nullable error, id __nullable data);
-typedef void (^YTKAsyncCallback) (NSArray * __nullable argument, YTKDataCallback block);
+typedef void (^YTKAsyncCallback) (NSArray * __nullable argument, __nullable YTKDataCallback block);
 typedef id _Nullable (^YTKSyncCallback) (NSArray * __nullable argument);
 typedef void (^YTKVoidSyncCallback) (NSArray * __nullable argument);
 typedef void (^YTKEventCallback) (NSArray * __nullable argument);
